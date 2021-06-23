c0cde00dc45a074d0a2642a9254f370ef4b0a364
Charles <chalamisterio@gmail.com>
2021-06-23 17:21:33 -0300

    em execução

---
+++ Aula36/script.js
@@ -0,0 +1,10 @@
+let foi1 = document.getElementById("age-table")
+
+console.log(foi,foi1,foi2)
+
+
+let label =document.getElementById("age-tbel")
+let input = document.querySelectorAll("label")
+
+
+
