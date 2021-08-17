ae6e688d9c741d88b89a38542c97e17e32ba4422
Charles David Duggen <charles.duggen@alunos.sc.senac.br>
2021-08-17 17:16:53 -0300

    em execução

--- Aula 62/pratica/database/index.js
+++ Aula 62/pratica/database/index.js
@@ -1,20 +1,32 @@
 require("dotenv").config();
-const {Sequelize} = require("sequelize");
+const { Sequelize } = require("sequelize");
 const dbConfig = require("../config/database");
+
+const Usuario = require("../modes/Usuarios");
+const Projeto = require("../modes/Projeto");
+const Endereco = require("../modes/Endereco");
 
 const sequelize = new Sequelize(dbConfig);
 
+// Inicializando os models
+Usuario.init(sequelize);
+Projeto.init(sequelize);
+Endereco.init(sequelize);
+
+// Definindo as associações para os models
+Usuario.associate(sequelize.models);
+Projeto.associate(sequelize.models);
+Endereco.associate(sequelize.models);
 
 module.exports = sequelize;
-//Testado Conexão
-//(async () =>{
-//try{
-  //  await sequelize.authenticate();
-    //console.log('Conexão bem sucediada');
-
-//}catch(erro){
- //   console.error('Conexão falhou!',erro);
-//}finally{
- //   sequelize.close();
-//}
-//})();
+// Testando conexão
+// (async () => {
+// try {
+// await sequelize.authenticate();
+// console.log('Conexão bem-sucedida!');
+// } catch (error) {
+// console.error('Conexão falhou!', error);
+// } finally {
+// sequelize.close();
+// }
+// })();
--- Aula 62/pratica/modes/Usuarios.js
+++ Aula 62/pratica/modes/Usuarios.js
@@ -1,9 +1,14 @@
-const { DataTypes } = require("sequelize");
+const { DataTypes, Sequelize, Model } = require("sequelize");
 const { hashSync, compareSync } = require("bcrypt");
-const sequelize = require("../database");
-const { UUID } = require("sequelize");
 
-const Usuario = sequelize.define("Usuario", {
+class Usuario extends Model {
+static init(sequelize) {
+super.init({
+id: {
+type: DataTypes.UUID,
+primaryKey: true,
+defaultValue: Sequelize.UUIDV4
+},
 nome: {
 type: DataTypes.STRING,
 allowNull: false
@@ -25,57 +30,29 @@ set(value) {
 this.setDataValue("senha", hashSync(value, 10));
 }
 }
-});
-// }, {
-// tableName: "usuarios",
-// underscored: true
-// });
-
-(async () => {
-try {
-// Criando a tabela Usuarios
-await Usuario.sync({ force: true });
-console.log("Tabela criada com sucesso!");
-
- // Inserindo um usuário
-const pedro = await Usuario.create({
-nome: "Pedro",
-email: "pedro@email.com",
-senha: "123456"
-});
-console.log("Usuário criado com sucesso");
-console.log(pedro.toJSON());
-
- // Modificar o pedro
-pedro.email = "pedrao@email.com";
-await pedro.save();
-console.log("E-mail do pedro atualizado!");
-
- // Remover o pedro
-await pedro.destroy();
-console.log("Pedro foi removido do banco de dados");
-
- // Inserindo vários usuários
-const usuarios = await Usuario.bulkCreate([
-{
-nome: "Pedro",
-email: "pedro@email.com",
-senha: "123456"
-},
-{
-nome: "José",
-email: "jose@email.com",
-senha: "123456"
+}, { sequelize });
 }
-]);
-console.log("Usuários inseridos com sucesso!");
-usuarios.forEach(usuario => console.log(usuario.toJSON()));
 
- // Comparando a senha
-console.log(compareSync("12346", usuarios[0].toJSON().senha));
-} catch (err) {
-console.error("Ocorreu algum erro ao criar a tabela", err);
-} finally {
-sequelize.close();
+ static associate(models) {
+this.belongsToMany(models.Projeto, {
+through: "UsuariosProjetos",
+as: {
+singular: "projeto",
+plural: "projetos"
 }
-})();
+});
+this.hasOne(models.Endereco, {
+foreignKey: {
+name: "idUsuario",
+allowNull: false
+}
+});
+}
+
+
+ checarSenha(senha) {
+return compareSync(senha, this.senha);
+}
+}
+
+module.exports = Usuario;
